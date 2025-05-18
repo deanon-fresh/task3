5f40e87d291ab27936e76ca4dca1954fd302e463
deanon-fresh <deanon.fresh@yandex.ru>
2025-05-18 14:23:38 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,28 +2,15 @@ import ru.netology.service.CustomService;
 import java.util.Scanner;
 
 public class Main {
+static int sum;
 
-
-    //выполнение 1 задания
-    /*public static int result;
-
-     public static int calculation2 (int price, int weight) {
-        result = price/100 + weight*100;
-        return result;
-    } */
-
-    public static void main(String[] SUM2) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите цену товара (в руб.) ");
         int price = scanner.nextInt();
         System.out.print("Введите вес товара (в кг.) ");
         int weight = scanner.nextInt();
-
-        //часть кода для 1 задания
-        //calculation2(price, weight);
-        //System.out.println("Размер пошлины в руб. составит " + result + " руб.");
-
-        CustomService.calculation(price, weight);
-        System.out.println("Размер пошлины в руб. составит " + SUM2 + " руб.");
+        sum = CustomService.calculation(price, weight);
+        System.out.println("Размер пошлины в руб. составит " + sum + " руб.");
     }
 }
